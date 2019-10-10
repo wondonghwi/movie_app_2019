796550e62eb54b255b5dbfc71e1e33f6b0a55f5f
wondonghwi <ehdehd1289@naver.com>
2019-10-10 16:30:34 +0900

    Updates

---
+++ precache-manifest.52c778297909d8e4e0f72ab264b0fb72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dbb878cba51e0b82652db34a7d678a",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "cca8eba6fa7d11b63698",
+    "url": "/movie_app_2019/static/css/main.b7cf6848.chunk.css"
+  },
+  {
+    "revision": "9c63e566ee8166e9431f",
+    "url": "/movie_app_2019/static/js/2.6d27e71f.chunk.js"
+  },
+  {
+    "revision": "cca8eba6fa7d11b63698",
+    "url": "/movie_app_2019/static/js/main.e1e6ecce.chunk.js"
+  },
+  {
+    "revision": "420037a5e08d7d6458d7",
+    "url": "/movie_app_2019/static/js/runtime-main.6a961451.js"
+  }
+]);
